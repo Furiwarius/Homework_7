80d681f8a4db304565c3e94cbfe11f5818f2f8d3
Vasiliy <vasiliy.trunov.00@mail.ru>
2022-08-30 12:10:45 +0200

    Задача 50

---
+++ program_050/Program.cs
@@ -0,0 +1,41 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//17 -> такого числа в массиве нет
+
+int[,] ArrayGenerator(){
+    Random random = new Random();
+    int[,] array = new int[random.Next(2,6), random.Next(2,6)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = random.Next(100);
+        }
+    }
+    return array;
+}
+
+void ValueCheck(int[,] array, int value){
+    int check = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i,j] == value) check=value;
+            System.Console.Write($"{array[i,j]} ");
+        }
+        System.Console.WriteLine();
+    }
+    if (check !=0) System.Console.WriteLine(check);
+    else  System.Console.WriteLine($"{value} -> такого числа в массиве нет");
+}
+
+int[,] newArray = ArrayGenerator();
+
+System.Console.Write("Введите число - ");
+int value1 = int.Parse(System.Console.ReadLine());
+
+ValueCheck(newArray, value1);
